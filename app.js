18dc5d4944cbf804be984a0858f429432d7d9a90
salman <alysalman15@gmail.com>
2023-10-24 23:03:03 +0500

    add image profile functionality

---
+++ app.js
@@ -0,0 +1,31 @@
+var userImage = document.getElementById("userImage");
+var changeProfile = document.getElementById("userProfile");
+
+
+function login() {
+    var userName = document.getElementById("username").value
+    var passWord = document.getElementById("pass").value
+
+
+    if (userName === "" || passWord === "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Username and password cant be empty!',
+        });
+    } else if (userName === 'smit' && password === 'smit') {
+        alert("Login Successfully!");
+        window.location.assign("start.html");
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please enter correct username and password!',
+        });
+    }
+}
+
+// for change profile image
+changeProfile.onchange = function () {
+    userImage.src = URL.createObjectURL(changeProfile.files[0])
+}

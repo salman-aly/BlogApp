8e6716e3329cd14ce084f12f30e1cd88c533791a
salman <alysalman15@gmail.com>
2023-10-25 17:19:16 +0500

    sign up registration in js

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 var userImage = document.getElementById("userImage");
 var changeProfile = document.getElementById("userProfile");
 
+var userName = document.getElementById("username").value
+var passWord = document.getElementById("pass").value
+
+// var signUpUsername = document.getElementById("username-signup").value
+// var signUppassword = document.getElementById("pass-signup").value
+
+// localStorage.setItem("username", JSON.stringify(signUpUsername));
+// localStorage.setItem("password", JSON.stringify(signUppassword));
+
+// var dataBaseUserName = JSON.parse(localStorage.getItem(signUpUsername));
+// var dataBasepassword = JSON.parse(localStorage.getItem(signUppassword));
+
+// console.log(dataBaseUserName)
+// console.log(dataBasepassword)
+
+var signUpUsername = document.getElementById("username-signup").value;
+var signUppassword = document.getElementById("pass-signup").value;
+
+localStorage.setItem("username", signUpUsername);
+localStorage.setItem("password", signUppassword);
+
+var dataBaseUserName = localStorage.getItem("username", signUpUsername);
+var dataBasepassword = localStorage.getItem("password", signUppassword);
+
+console.log(dataBaseUserName);
+console.log(dataBasepassword);
+
+
+
 // for login restriction
 function login() {
-    var userName = document.getElementById("username").value
-    var passWord = document.getElementById("pass").value
-
-
     if (userName === "" || passWord === "") {
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
             text: 'Username and password cant be empty!',
         });
-    } else if (userName === 'smit' && password === 'smit') {
+    } else if (userName === dataBaseUserName && passWord === dataBasepassword) {
         alert("Login Successfully!");
         window.location.assign("start.html");
     } else {
@@ -32,7 +57,33 @@ changeProfile.onchange = function () {
     userImage.src = URL.createObjectURL(changeProfile.files[0])
 }
 
-// function for sign up form
-function signUP() {
 
+// function for sign up form
+function signUp() {
+    var signUpUsername = document.getElementById("username-signup").value
+    var signUppassword = document.getElementById("pass-signup").value
+
+    if (signUpUsername === "" || signUppassword === "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'You cant signup without username and password!',
+        });
+    }
+
+    // console.log("Username localStorage:", signUpUsername);
+    // console.log("Password localStorage:", signUppassword);
+
+}
+
+// function for register form
+function register() {
+    var signup = document.getElementById("signUp").style.display = "block"
+    var login = document.getElementById("form").style.display = "none"
+}
+
+// function for close sign up form
+function closePopUp() {
+    var signup = document.getElementById("signUp").style.display = "none"
+    var login = document.getElementById("form").style.display = "flex"
 }

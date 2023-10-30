19548aa33bb6a384b1ad37f72e79b5c39dd2d106
salman <alysalman15@gmail.com>
2023-10-30 11:09:53 -0500

    quil editor add

--- post.js
+++ post.js
@@ -1,3 +1,6 @@
+var quill = new Quill('#editor', {
+  theme: 'snow'
+});
 //User create post 
 var userPost = document.getElementById("userPost");
 //For profile change
@@ -8,7 +11,7 @@ var changeProfile = document.getElementById("userProfile");
 var userName = document.getElementById("username");
 var passWord = document.getElementById("pass");
 
-var showPost = document.getElementById("display");
+var showPost = document.getElementById("editor");
 
 // var postUserName = document.getElementById("changePostName");
 
@@ -36,8 +39,8 @@ userPost.innerHTML = `
         <div id="changePostUsername">@${dataBaseUserName}22</div>
       </div>
     </div>
-    <div>
-      <input type="text" id="display" placeholder="Share your thoughts ${dataBaseUserName}?" />
+    <div id="editor">
+      
     </div>
     <div class="icons">
       <i class="bx bx-image-add" style="color: #007fff" id="bx" onclick="bgColorChange()"></i>
@@ -46,13 +49,13 @@ userPost.innerHTML = `
       <i class='bx bx-smile' style='color:#007fff' ></i>
       <i class='bx bxs-location-plus' style='color:#007fff' ></i>
     </div>
-    <button class="postBtn" onclick="post()">Post</button>
+    
   </div>
 `
 
 
 function post() {
-  var showPost = document.getElementById("display").value
+  var showPost = document.getElementById("editor").value
 
   if (showPost === "") {
     postedPost.style.display = "none";
@@ -64,7 +67,7 @@ function post() {
     postedPost.innerHTML += `
       <div class="newPost createdPost">
         <h3>Published Blog</h3>
-        <p>${display.value}</p>
+        <p>${quill.getText()}</p>
         <div class="social">
           <i class="bx bx-like" style="color: #ffffff"></i>
           <i class="bx bxs-chat" style="color: #ffffff"></i>
@@ -72,8 +75,8 @@ function post() {
         </div>
       </div>
     `
-    localStorage.setItem('poststore', showPost);
-    document.getElementById("display").value = ""
+    localStorage.setItem('poststore', quill.getText());
+    document.getElementById("editor").value = ""
   }
 }
 
